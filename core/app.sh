c86f117f60f7ff29eb5b8e0bf895ece8ed8a7a79
wiktorkisielewski <wiktorkisielewski@protonmail.com>
2023-01-15 12:44:29 +0100

    postgres-retention-sidecar:0.0.6

--- core/app.sh
+++ core/app.sh
@@ -4,3 +4,8 @@ while :
     do cat targets.yml
     sleep 10
 done
+
+
+get_table_names() {
+  psql -U $PGUSER -h $PG_HOST -d bb_icmarkets -c "SELECT tablename FROM pg_catalog.pg_tables WHERE tableowner='$PGUSER';" | tail -n +3 | head -n -2
+}
