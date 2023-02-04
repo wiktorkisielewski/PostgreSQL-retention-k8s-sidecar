3910a594935d211b1475fa1468c998842dc6645c
wiktorkisielewski <wiktorkisielewski@protonmail.com>
2023-02-04 18:23:56 +0100

    postgres-retention-sidecar:0.1.4

--- core/scripts/app.sh
+++ core/scripts/app.sh
@@ -10,7 +10,7 @@ while :
   target_databases=$(get_target_databases $TARGETS_LIST)
   echo "Targeting databases: $target_databases"
   for database in $target_databases;
-    do target_tables=$(get_target_tables $TARGET_LIST $database)
+    do target_tables=$(get_target_tables $TARGETS_LIST $database)
     echo "Clearing tables: $target_tables in db: $database";
   done;
   sleep $RETENTION_FREQUENCY
